a08eadb47083052bac7a31e293b546fe926db9ae
Valerie Sobol <vssobol@gmail.com>
2019-05-16 21:18:56 -0700

    problem 1 passes

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-  
+function sum(a, b) {
+    var add = a + b;
+    var sentence = "The sum of " + a + " and " + b + " is " + add + ".";
+    return[add, sentence];
 }
 
 // Here is the test for sum(); uncomment it to run it
